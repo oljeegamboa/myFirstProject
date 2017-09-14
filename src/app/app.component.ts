d62f8ff7562860a5151e2b6ddab580b2fddb9c77
oljeegamboa <30281163+oljeegamboa@users.noreply.github.com>
2017-09-14 14:44:06 +0800

    semi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+title = 'app';
 
-
-studentCollection: Array<Object> = [];
-studentRecord:Object;
+studentCollection: Array<object> = [];
+studentRecord:object;
 
 studNo:number;
 studFname:string;
@@ -18,59 +17,63 @@ studLname:string;
 studProg:string;
 studYr:number;
 
-
-messages='';
-printing=false;
+messages= '';
+printing= false;
 
 private checkPatterns(value:any , pattern:RegExp):
 boolean{
-  if(pattern.test(value))
-    return true;
-  else
-    return false;
+if(pattern.test(value))
+  return true;
+else 
+  return false;
+
+
+}
+addStudentEntry(): Boolean{
+  this.printing = false;
+  const stringPattern = /^[A-z\s]+$/;
+  const studNumberPattern =/^[0-9]+$/;
+  const studYearPattern= /^[1-5]+$/;
+
+  if(this.checkPatterns(this.studNo,studNumberPattern) &&
+  this.checkPatterns(this.studLname,stringPattern) && this.checkPatterns(this.studProg,stringPattern)
+  && this.checkPatterns(this.studYr,studYearPattern)
+  )
+this.studentRecord = {
+  studNumber: this.studNo,
+  studFirstName: this.studFname,
+  studLastname: this.studLname,
+  studProgram: this.studProg,
+  studYear: this.studYr
+}
+
+
+  else{
+this.messages = 'Errors have been encountered and therefore cannot continue to process requested operation.';
+return false;
+
+  }
+}
+
+listStudents(): void{
+  this.printing = true;
+  console.log('Showing stored students');
+
+}
+
+clearValues():void{
+this.studNo=null;
+this.studFname=null;
+this.studLname=null;
+this.studProg=null;
+this.studYr=null;
 
 }
 
 
-addStudentEntry(): Boolean{
 
 
-  this.printing = false;
-  const stringPattern = /^[A-z\s]+$/;
-  const studNumberPattern = /^[0-9]+$/;
-  const studYearPattern = /^[1-5]+$/;
-
-  if(this.checkPatterns(this.studNo,studNumberPattern) &&
-    this.checkPatterns(this.studLname,stringPattern) &&
-    this.checkPatterns(this.studProg,stringPattern) &&
-    this.checkPatterns(this.studYr,studYearPattern))
-
-    this.studentRecord = {
-      studNumber: this.studNo,
-      studFirstName:this.studFname,
-      studLastName: this.studLname,
-      studProgram: this.studProg,
-      studYear: this.studYr
-  }
-
-    else{
-      this.messages = 'Errors have been encountered and therefore cannot continue to process requested operation.';
-      return false;
-    }
-  
-  }
-  listStudents (): void{
-    this.printing = true;
-    console.log('Showing stored students')
-  }
-
-  clearValues():void{
-    this.studNo=null;
-    this.studFname=null;
-    this.studLname=null;
-    this.studProg=null;
-    this.studYr;
-  }
 
 
-    }
+
+}
